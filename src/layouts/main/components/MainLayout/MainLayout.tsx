7118d511dbefd70c00359485cc9486ae1aac0f66
unknown <33asasergey33@gmail.com>
2022-01-07 17:55:41 +0300

    fix: change text import

--- src/layouts/main/components/MainLayout/MainLayout.tsx
+++ src/layouts/main/components/MainLayout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { MainHeader } from '../MainHeader';
 
 import { MainFooter } from '../MainFooter';
 import { Box } from '@/common/components/system/Box';
-import { Paragraph } from '@/common/components/system/Text';
+import * as Text from '@/common/components/system/Text';
 
 import * as S from './styled';
 
@@ -32,16 +32,16 @@ export const MainLayout = (props: MainLayoutProps): JSX.Element => {
         marginRight="auto"
       >
         <MainFooter>
-          <Paragraph
+          <Text.Paragraph
             fontSize="inherit"
           >
             LTeam.gg isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties.
-          </Paragraph>
-          <Paragraph
+          </Text.Paragraph>
+          <Text.Paragraph
             fontSize="inherit"
           >
             Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
-          </Paragraph>
+          </Text.Paragraph>
         </MainFooter>
       </Box>
     </S.MainLayoutContainer>
