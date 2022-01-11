d6eba7dc43ff204fb9dfa5377bc4b054800cbce4
unknown <33asasergey33@gmail.com>
2022-01-11 19:31:11 +0300

    fix: change LocalStorage style

--- src/modules/summoner/components/LocalStorageSummoners/LocalStorageSummoners.tsx
+++ src/modules/summoner/components/LocalStorageSummoners/LocalStorageSummoners.tsx
@@ -16,8 +16,96 @@ import { IconButton } from '@/common/components/system/IconButton';
 import { Stack } from '@/common/components/system/Stack';
 import * as Text from '@/common/components/system/Text';
 
-import * as S from './styled';
 import { RecentSummoner } from '../../interfaces/summoner.interface';
+
+interface SummonersProps {
+  summonersList: RecentSummoner[];
+  title: 'Recent' | 'Favorite';
+  onStar?: (data: RecentSummoner) => void;
+  onDelete: (data: RecentSummoner) => void;
+}
+
+const Summoners = (props: SummonersProps) => {
+  const {
+    summonersList,
+    title,
+    onStar,
+    onDelete,
+  } = props;
+  return (
+    <div>
+      <Text.Paragraph>{title}</Text.Paragraph>
+
+      <Stack
+        component="ul"
+        direction="column"
+      >
+        {summonersList.map((data) => {
+          return (
+            <Box
+              key={data.id}
+              backgroundColor="radix.gray1"
+              display="flex"
+              padding={2}
+              width="100%"
+              alignItems="center"
+              justifyContent="flex-start"
+              borderRadius={4}
+            >
+              <Box
+                display="flex"
+                alignItems="center"
+                flex={1}
+              >
+
+                <Box
+                  display="flex"
+                  minWidth={96}
+                  alignItems="center"
+                >
+                  <RiHistoryFill size={24} color="inherit" />
+
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    padding="4px 8px"
+                    marginLeft={3}
+                    backgroundColor="radix.gray5"
+                  >
+                    {data.region.toUpperCase()}
+                  </Box>
+                </Box>
+
+                <Link href={`/${data.region}/summoner/${data.name}`} passHref>
+                  <Text.Paragraph
+                    component="a"
+                    variant="body2"
+                  >
+                    {data.name}
+                  </Text.Paragraph>
+                </Link>
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="center"
+                marginLeft="auto"
+              >
+                <IconButton color="inherit" onClick={() => onStar?.(data)}>
+                  {title === 'Recent' ? <RiStarLine size={24} /> : <RiStarFill size={24} color="gold" />}
+                </IconButton>
+                <IconButton color="inherit" onClick={() => onDelete(data)}>
+                  <IoMdClose size={24} />
+                </IconButton>
+              </Box>
+            </Box>
+          );
+        })}
+      </Stack>
+    </div>
+  );
+};
 
 export const LocalStorageSummoners = () => {
   const [recentSummoners, setRecentSummoners, deleteRecentSummoner] = useRecentSummoners();
@@ -28,6 +116,14 @@ export const LocalStorageSummoners = () => {
     deleteRecentSummoner(data);
   };
 
+  const deleteFromRecent = (data: RecentSummoner) => {
+    deleteRecentSummoner(data);
+  };
+
+  const deleteFromFavorite = (data: RecentSummoner) => {
+    deleteFavoriteSummoner(data);
+  };
+
   return (
     <DisplayOnBrowserMount>
       {((recentSummoners.length > 0) || (favoriteSummoners.length > 0)) && (
@@ -35,152 +131,24 @@ export const LocalStorageSummoners = () => {
         padding={3}
         backgroundColor="radix.gray3"
       >
-        {recentSummoners.length > 0 && (
-          <>
-            <Text.Paragraph>Recent</Text.Paragraph>
+        <Stack direction="column">
 
-            <Stack component={S.Ul} direction="column">
-              {recentSummoners.map((data) => {
-                return (
-                  <Box
-                    component="li"
-                    key={data.id}
-                    backgroundColor="radix.gray1"
-                    display="flex"
-                    padding={2}
-                    width="100%"
-                    alignItems="center"
-                    justifyContent="flex-start"
-                    borderRadius={4}
-                  >
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      flex={1}
-                    >
-
-                      <Box
-                        display="flex"
-                        minWidth={96}
-                        alignItems="center"
-                      >
-                        <RiHistoryFill size={24} color="inherit" />
-
-                        <Box
-                          display="flex"
-                          justifyContent="center"
-                          alignItems="center"
-                          padding="4px 8px"
-                          marginLeft={3}
-                          backgroundColor="radix.gray5"
-                        >
-                          {data.region.toUpperCase()}
-                        </Box>
-                      </Box>
-
-                      <Link href={`/${data.region}/summoner/${data.name}`} passHref>
-                        <Text.Paragraph
-                          component="a"
-                          variant="body2"
-                        >
-                          {data.name}
-                        </Text.Paragraph>
-                      </Link>
-                    </Box>
-                    <Box
-                      display="flex"
-                      justifyContent="flex-end"
-                      alignItems="center"
-                      marginLeft="auto"
-                    >
-                      <IconButton color="inherit">
-                        <RiStarLine size={24} onClick={() => addToFavorite(data)} />
-                      </IconButton>
-                      <IconButton color="inherit" onClick={() => deleteRecentSummoner(data)}>
-                        <IoMdClose size={24} />
-                      </IconButton>
-                    </Box>
-                  </Box>
-                );
-              })}
-            </Stack>
-          </>
-        )}
-        <Box
-          marginTop={3}
-        >
-          {favoriteSummoners.length > 0 && (
-            <>
-              <Text.Paragraph>Favorite</Text.Paragraph>
-              <S.Ul>
-                {favoriteSummoners.map((data) => {
-                  return (
-                    <S.Li
-                      key={data.id}
-                    >
-                      <Box
-                        display="flex"
-                        padding={2}
-                        width="100%"
-                        alignItems="center"
-                        justifyContent="flex-start"
-                        backgroundColor="radix.gray1"
-                        borderRadius={4}
-                      >
-                        <Box
-                          display="flex"
-                          alignItems="center"
-                          flex={1}
-                        >
-
-                          <Box
-                            display="flex"
-                            minWidth={96}
-                            alignItems="center"
-                          >
-                            <RiHistoryFill size={24} color="inherit" />
-
-                            <Box
-                              display="flex"
-                              justifyContent="center"
-                              alignItems="center"
-                              backgroundColor="radix.gray5"
-                              padding="4px 8px"
-                              marginLeft={3}
-                            >
-                              {data.region.toUpperCase()}
-                            </Box>
-                          </Box>
-
-                          <Link href={`/${data.region}/summoner/${data.name}`} passHref>
-                            <Text.Paragraph
-                              component="a"
-                              variant="body2"
-                            >
-                              {data.name}
-                            </Text.Paragraph>
-                          </Link>
-                        </Box>
-                        <Box
-                          display="flex"
-                          justifyContent="flex-end"
-                          alignItems="center"
-                          marginLeft="auto"
-                        >
-                          <RiStarFill size={24} color="gold" />
-
-                          <IconButton color="inherit" onClick={() => deleteFavoriteSummoner(data)}>
-                            <IoMdClose size={24} />
-                          </IconButton>
-                        </Box>
-                      </Box>
-                    </S.Li>
-                  );
-                })}
-              </S.Ul>
-            </>
+          {recentSummoners.length > 0 && (
+            <Summoners
+              summonersList={recentSummoners}
+              title="Recent"
+              onStar={addToFavorite}
+              onDelete={deleteFromRecent}
+            />
           )}
-        </Box>
+          {favoriteSummoners.length > 0 && (
+            <Summoners
+              summonersList={favoriteSummoners}
+              title="Favorite"
+              onDelete={deleteFromFavorite}
+            />
+          )}
+        </Stack>
       </Box>
       )}
     </DisplayOnBrowserMount>
